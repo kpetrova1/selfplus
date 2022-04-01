3052925d1e246fe8427a12c1845d2196eb7d7d91
kristina <hamsterart.biz@gmail.com>
2022-04-01 12:02:20 +0600

    add files

--- gulp-tasks/copy-files.js
+++
@@ -1,9 +0,0 @@
-import {path, src} from './path.js';
-import gulp from 'gulp';
-import sync from 'browser-sync';
-
-export default function copy() {
-    return  gulp.src(path.files.src, {allowEmpty: true})
-        .pipe(gulp.dest(path.files.dist))
-        .on('end', sync.reload);
-};
--- gulp-tasks/fonts.js
+++
@@ -1,9 +0,0 @@
-import {path} from './path.js';
-import gulp from 'gulp';
-import ttf2woff from 'gulp-ttf2woff';
-import ttf2woff2 from 'gulp-ttf2woff2';
-
-export default function fonts() {
-    gulp.src(path.fonts.src).pipe(ttf2woff()).pipe(gulp.dest(path.fonts.dist));
-    return gulp.src(path.fonts.src).pipe(ttf2woff2()).pipe(gulp.dest(path.fonts.dist));
-}
