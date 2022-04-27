278a10741333f93047baaa0d1842364d2f5d3e79
Kristina <hamsterart7@gmail.com>
2022-04-27 12:17:17 +0600

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
   $('.tab__btn').on('click', function () {
     if ($(this).parent().hasClass('tab--active')) {
       $(this).parent().removeClass('tab--active');
-      $(this).siblings('.tab__body').slideUp(200);
+      $(this).siblings('.tab__body').slideUp(400);
     } else {
       $('.tab__btn').parent().removeClass('tab--active');
       $('.tab-img').removeClass('tab-img--active');
       $(this).parent().addClass('tab--active');
       const tabNum = $(this).parent().attr('data-tab');
       $('.tab-img[data-tab="' + tabNum + '"]').addClass('tab-img--active');
-      $('.tab__body').slideUp(200);
-      $(this).siblings('.tab__body').slideDown(200);
+      $('.tab__body').slideUp(400);
+      $(this).siblings('.tab__body').slideDown(400);
     }
   });
 
